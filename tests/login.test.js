f9ee8c58c8ef9fe448551c81277e1634266c39f0
Hugo Torquetti <112561756+TorksCoder@users.noreply.github.com>
2026-03-23 23:59:43 -0300

    Adicionando o arquivo inicial com teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,39 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6'; // Incluímos o check nessa importação pois não precisamos dar o comando de importação duas vezes
+
+
+export const options = {
+  // Defina o número de iterações para o teste
+  iterations: 50,
+  // Define os thresholds de critérios para cada métrica
+  thresholds: {
+    http_req_duration: ['p(90)<20', 'max<1'],
+    http_req_failed: ['rate<0.01']
+  }
+};
+
+export default function () {
+    // Tudo o que estiver aqui dentro será o nosso teste
+    const url = 'http://localhost:3000/login';
+    
+    const payload = JSON.stringify({
+        username: 'julio.lima',
+        senha: '123456',
+    });
+
+    const params = {
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    };
+
+    const res = http.post(url, payload, params);    // Colocamos a resposta da nossa requisição dentro de uma variável.
+
+    // Aqui validamos a resposta utilizando o check, que é uma função do k6 que nos permite fazer asserções.
+    check(res, { // O check tem a seguinte estrutura: check(resposta, { 'descrição do teste': (resposta) => condição para validar a resposta })
+        'Validar que o status é 200': (r) => r.status === 200, // o r aqui faz referência ao res que passamos como primeiro atributo na linha acima.
+        'Validar que o token é string': (r) => typeof (r.json().token) == 'string',
+    });
+
+    sleep(1);
+}
